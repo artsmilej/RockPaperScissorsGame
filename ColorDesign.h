5447bdd0af2c3ddd5d7248c900397cc59da8c296
artsmilej <artemus200918@gmail.com>
2023-06-08 19:57:09 +0300

    deleted colordesign

--- ColorDesign.h
+++ ColorDesign.h
@@ -1,13 +1,8 @@
 #pragma once
-#define BLK L"\033[0;30m" //Black
-#define RED L"\033[0;31m" //Red
 #define GRN L"\033[0;32m" //Green
 #define YEL L"\033[0;33m" //Yellow
 #define BLU L"\033[0;34m" //Blue
-#define MAG L"\033[0;35m" //Magenta
-#define CYN L"\033[0;36m" //Cyan
 #define WHT L"\033[0;37m" //White
-#define DGR L"\033[0;2m" //Dark grey
 #define ColorBLU std::cout << "\033[0;34m";
 #define ColorGRN std::cout << "\033[0;32m";
 #define ColorYEL std::cout << "\033[0;33m";
